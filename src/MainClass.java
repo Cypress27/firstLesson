f397a90c37dcc151cb338406be2fcc34db00f8b2
Cypress27 <konstantin.verner@mail.ru>
2021-08-16 00:24:27 +0700

    First lesson test 3

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,7 @@
 public class MainClass {
 
     private int class_number = 20;
+    private String class_string = new String("Hello, world");
 
     public int getClassNumber() {
         return class_number;
@@ -9,5 +10,9 @@ public class MainClass {
     public int getLocalNumber() {
         int number = 14;
         return number;
+    }
+
+    public String getClassString() {
+        return class_string;
     }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -13,4 +13,11 @@ public class MainClassTest {
         MainClass classNumber = new MainClass();
         Assert.assertTrue("Class number <= 45",classNumber.getClassNumber()>45);
     }
+
+    @Test
+    public void testGetClassString() {
+        MainClass classString = new MainClass();
+        Assert.assertTrue("Class string doesn't contain hello/Hello",
+                classString.getClassString().contains("Hello")||classString.getClassString().contains(("hello")));
+    }
 }
